ce805beee496a8423275b00c33d75460b864d140
StanleyIG <oootehts@gmail.com>
2023-07-20 11:46:56 +0300

    add hh_ru parser

--- HH_ru_bs4/hh_var3.py
+++ HH_ru_bs4/hh_var3.py
@@ -19,24 +19,23 @@ class HHruParser:
             f'currency_code=RUR&experience=doesNotMatter&order_by=relevance&search_period=0&items_on_page=20'
         self.session = requests.Session()
 
-
     """получает статус код"""
+
     def get_status_code(self):
         response = self.session.get(self.url, headers=self.headers)
         if response.status_code == 200:
             return True
         return False
 
-
     """функция постраничной обработки вакансий c использованием матчинга
        что делает код более гибким к изменениям на сайте, и позволяет легко добавлять другие условия
     """
+
     def get_vacansy(self, content):
         soup = bs(content.content, 'html.parser')
         block = soup.find_all(
             'div', {'class': 'vacancy-serp-item-body__main-info'})
         vacancies = []
-        #salary_lst = []
         for i in block:
             sal_raw = i.findChild(
                 'span', {'class': 'bloko-header-section-3'})
@@ -49,7 +48,8 @@ class HHruParser:
                 if len(is_digit) < 2:
                     is_digit_count_1 = is_digit[0]
                 elif len(is_digit) > 1:
-                    is_digit_count_2 = is_digit[1] 
+                    is_digit_count_2 = is_digit[1]
+                salary_min, salary_max = None, None
                 match sal_raw.split():
                     case ['от', is_digit_count_1, currency] if is_digit_count_1 and currency:
                         salary_min, salary_max = salary[0], None
@@ -58,19 +58,19 @@ class HHruParser:
                     case ['до', is_digit_count_1, currency] if is_digit_count_1 and currency:
                         salary_min, salary_max = None, salary[0]
                     case ['от', is_digit_count_1, '–', 'до', is_digit_count_2, currency] if is_digit_count_1 \
-                        and is_digit_count_2 and currency:
+                            and is_digit_count_2 and currency:
                         salary_min, salary_max = salary[0], salary[1]
                     case ['от', is_digit_count_1, currency, 'до', deduction, taxes] if is_digit_count_1 and currency \
-                          and deduction and taxes:
-                        salary_min, salary_max = salary[0], salary[1]
+                            and deduction and taxes:
+                        salary_min, salary_max = salary[0], None
                     case ['до', is_digit_count_1, currency, 'до', deduction, taxes] if is_digit_count_1 and currency \
-                          and deduction and taxes:
+                            and deduction and taxes:
                         salary_min, salary_max = salary[0], None
 
             else:
                 salary_min, salary_max = None, None
                 currency = None
-            
+
             vacancy_text = i.findChild(
                 'a', {'class': 'serp-item__title'}).text
             vacancy_href = i.findChild(
@@ -84,8 +84,8 @@ class HHruParser:
             vacancies.append(dct_block)
         return vacancies
 
-
     """получение всех вакансий"""
+
     def get_all_vacancies(self):
         status = self.get_status_code()
         vacancy_list = []
@@ -96,23 +96,21 @@ class HHruParser:
         content = bs(res.text, 'html.parser')
         total_vacancies = content.find(
             'div', {"data-qa": "vacancies-search-header"}).findChild('h1').contents[0]
-        
+
         for i in range(self.max_page):
             response = self.session.get(
                 self.url, headers=self.headers, params=params)
             if not self.get_vacansy(response):
                 break
-            salary_lst = self.get_vacansy(response)
             vacancy_list.extend(self.get_vacansy(response))
             params['page'] += 1
 
             print(f"обработано страниц: {params['page']}")
         print(f"всего вакансий: {total_vacancies}")
         return vacancy_list
-    
-
 
     """записывает вакансии в json"""
+
     def write_json(self):
         FILENAME = 'HH_ru_bs4/test.json'
         with open(FILENAME, 'w', encoding='utf-8') as file:
@@ -121,12 +119,11 @@ class HHruParser:
         with open(FILENAME, 'r', encoding='utf-8') as file:
             data = len(json.load(file))
             return data
-        
-
 
     """выводит результат через DataFrame pandas"""
+
     def to_pandas(self):
-        FILENAME = 'HH_ru_bs4/hhru.json'
+        FILENAME = 'HH_ru_bs4/test.json'
         if os.path.exists(FILENAME):
             with open(FILENAME, 'r', encoding='utf-8') as file:
                 data = json.load(file)
@@ -137,10 +134,9 @@ class HHruParser:
             with open(FILENAME, 'r', encoding='utf-8') as file:
                 data = json.load(file)
             return pd.DataFrame(data)
-    
+
 
 hhru = HHruParser('python', 11)
-##pp(hhru.get_all_vacancies())
-#hhru.write_json()
-print(hhru.to_pandas())
-
+# pp(hhru.get_all_vacancies())
+hhru.write_json()
+# print(hhru.to_pandas())
